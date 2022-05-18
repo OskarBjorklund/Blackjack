929ff53056e16d443a9442f354e05f79e66f3753
OskarBjorklund <oskar.bjorklund@skola.taby.se>
2022-05-18 22:32:14 +0200

    Fixat mycket
    Behöver satsa och behöver se snyggare ut

--- Blackjack.js
+++ Blackjack.js
@@ -1,4 +1,4 @@
-
+let kan_hit = true;
 
 function ny_kortlek(){
   let kortlek = new Kortlek();
@@ -12,11 +12,11 @@ function ny_kortlek(){
   return kortlek;
 }
 
+
 function värdera(kort){
   let handsumma = 0;
   let antal_ess = 0;
   for (let i = 0; i <= kort.length - 1; i++){
-    console.log(kort[i]);
     if (kort[i].värde == "Ess"){
       antal_ess += 1;
     }
@@ -24,7 +24,7 @@ function värdera(kort){
     handsumma += kort[i].värde;
     }
   }
-  for (let i = 0; i < antal_ess; i++){
+  for(let i = 0; i < antal_ess; i++){
     if (handsumma+11*(antal_ess-i) > 21){
       handsumma += 1;
     }
@@ -36,21 +36,86 @@ function värdera(kort){
   return handsumma;
 }
 
-function starta_spel(){
+
+window.onload = function starta_spel(){
   let kortlek = ny_kortlek();
 
   kortlek.blanda()
 
   let dealer_hand = [];
   let spelare_hand = [];
-  
-  for(let i = 0; i < 2; i++){
-    dealer_hand.push(kortlek.dra_kort());
-    spelare_hand.push(kortlek.dra_kort());
-  }
-}
 
-starta_spel()
+  let gömt_kort = kortlek.dra_kort();
+  dealer_hand.push(gömt_kort);
+
+  while (värdera(dealer_hand) < 17){
+    let kortImg = document.createElement("img");
+    let kort = kortlek.dra_kort();
+    dealer_hand.push(kort);
+
+    kortImg.src = "./cards/"+ kort.valör + "-" + kort.färg + ".png"
+    document.getElementById("dealer_kort").append(kortImg)
+  }
+  console.log(värdera(dealer_hand))
+
+  for (let i = 0; i < 2; i++){
+    let kortImg = document.createElement("img");
+    let kort = kortlek.dra_kort();
+    spelare_hand.push(kort);
+    
+    kortImg.src = "./cards/"+ kort.valör + "-" + kort.färg + ".png"
+    document.getElementById("spelare_kort").append(kortImg)
+    document.getElementById("spelare_summa").innerText = värdera(spelare_hand);
+  }
+
+  document.getElementById("hit").addEventListener("click", hit);
+  document.getElementById("stanna").addEventListener("click", stanna);
+
+
+  function hit() {
+    if (!kan_hit){
+      return;
+    }
+    let kortImg = document.createElement("img");
+    let kort = kortlek.dra_kort();
+    spelare_hand.push(kort);
+      
+    kortImg.src = "./cards/"+ kort.valör + "-" + kort.färg + ".png"
+    document.getElementById("spelare_kort").append(kortImg);
+    document.getElementById("spelare_summa").innerText = värdera(spelare_hand);
+
+    if (värdera(spelare_hand) > 21){
+      kan_hit = false;
+      console.log(värdera(spelare_hand));
+    }
+  }
+
+  function stanna(){
+    kan_hit = false;
+    document.getElementById("gömt").src = "./cards/"+ dealer_hand[0].valör + "-" + dealer_hand[0].färg + ".png";
+
+    let resultat = "";
+    if (värdera(spelare_hand) > 21){
+      resultat = "Du förlorade!"
+    }
+    else if (värdera(dealer_hand) > 21){
+      resultat = "Du vann!"
+    }
+    else if (värdera(spelare_hand) == värdera(dealer_hand)){
+      resultat = "Oavgjort!"
+    }
+    else if (värdera(spelare_hand) > värdera(dealer_hand)){
+      resultat = "Du vann!"
+    }
+    else if (värdera(spelare_hand) < värdera(dealer_hand)){
+      resultat = "Du förlorade!"
+    }
+    document.getElementById("dealer_summa").innerText = värdera(dealer_hand);
+    document.getElementById("spelare_summa").innerText = värdera(spelare_hand);
+    document.getElementById("resultat").innerText = resultat;
+  }
+  
+}
 
 
 //spelare_hand.push(kortlek.dra_kort()); 
